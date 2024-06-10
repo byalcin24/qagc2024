5e2f2cd1bb7a27fec658120b27c3893d781ff805
Batu Yalcin <batu.yalcin@gmail.com>
2024-06-10 20:32:29 +0300

    add pre-ansatz

--- app/src/utils/hamiltonian.py
+++ app/src/utils/hamiltonian.py
@@ -1,6 +1,6 @@
 from challange_2024 import problem_hamiltonian
 
-n_qubits = 28
+n_qubits = 4
 
 ham = problem_hamiltonian(n_qubits, 1, hamiltonian_directory="app/src/hamiltonian/data")
-print(ham)
+print(type(ham))
